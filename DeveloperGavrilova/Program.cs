63bd3ec40b6560976e0483e43c8403701aa2a12c
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2022-12-06 11:16:14 +0300

    описана функция для создания массива строк

--- DeveloperGavrilova/Program.cs
+++ DeveloperGavrilova/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string[] CreateStringArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
